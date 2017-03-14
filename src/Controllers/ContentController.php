5c4164f387f4784d6af974ce6d912c97ccf00037
domee1337 <a2@fs24.local>
2017-03-14 09:22:22 +0100

    z

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -8,7 +8,7 @@ use Plenty\Plugin\Templates\Twig;
  
 class ContentController extends Controller
 {
-    ublic function sayHello(Twig $twig):string
+    public function sayHello(Twig $twig):string
     {
         return $twig->render('HelloWorld::content.hello');
     }
