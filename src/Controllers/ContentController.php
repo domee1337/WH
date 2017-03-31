203d5d36fbf1f05a88dab413a80393913829fd1b
domee1337 <a2@fs24.local>
2017-03-31 14:18:22 +0200

    a

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -93,7 +93,7 @@ class ContentController extends Controller
     {
       $varRepo->hasBarcode("4260177462537");
       $result = $varRepo->lookup();
-      return json_encode($request);
+      return json_encode($request->all());
 
     }
 }
