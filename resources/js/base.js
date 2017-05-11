231a3e300a0377669cfcd29f4434286d71a75aa7
domee1337 <a2@fs24.local>
2017-05-11 12:09:33 +0200

    nn

--- resources/js/base.js
+++ resources/js/base.js
@@ -2,13 +2,19 @@ function findVariant(barcode)
 {
   $.ajax({
         type: "GET",
-        url: "calls/findArticle",
+        url: "warehouse/calls/findArticle",
         data: {barcode: barcode},
         success: function(data)
         {
+            data = $.parseJSON(data);
+
             if(data.valid)
             {
-              $('#output').html("<p>Artikel "+data.number+" wurde gefunden.</p>");
+              $('#output').html("");
+              $.each( data.variants, function(variant){
+                $('#output').append("<p>Artikel "+variant.number+" wurde gefunden.</p>");
+              });
+              
             }
             else {
               $('#output').html("<p>Es wurde keine Artikel gefunden.</p>");
