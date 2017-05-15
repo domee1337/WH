7c0fc1af8c54f4f478793a3ab5c3cf1ff02c6cfa
domee1337 <a2@fs24.local>
2017-05-15 10:12:54 +0200

    einbuchen funkio

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,3 +1,46 @@
+Date.prototype.toW3CString = function () {
+    var year = this.getFullYear();
+    var month = this.getMonth();
+    month ++;
+    if (month < 10) {
+        month = '0' + month;
+    }
+    var day = this.getDate();
+    if (day < 10) {
+        day = '0' + day;
+    }
+    var hours = this.getHours();
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    var minutes = this.getMinutes();
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    var seconds = this.getSeconds();
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    var offset = -this.getTimezoneOffset();
+    var offsetHours = Math.abs(Math.floor(offset / 60));
+    var offsetMinutes = Math.abs(offset) - offsetHours * 60;
+    if (offsetHours < 10) {
+        offsetHours = '0' + offsetHours;
+    }
+    if (offsetMinutes < 10) {
+        offsetMinutes = '0' + offsetMinutes;
+    }
+    var offsetSign = '+';
+    if (offset < 0) {
+        offsetSign = '-';
+    }
+    return year + '-' + month + '-' + day +
+        'T' + hours + ':' + minutes + ':' + seconds +
+        offsetSign + offsetHours + ':' + offsetMinutes;
+};
+
+
+
 var variationId;
 
 function findVariant(barcode)
@@ -86,12 +129,14 @@ function login()
 
 function menge()
 {
+  $('.back').removeAttr("disabled");
   if($("#standart_menge").is(':checked'))
   {
     $('.locationEan').removeAttr("disabled");
     $('.locationEan').select();
   }
   else {
+    $('.locationEan').removeAttr("disabled");
     $('#menge').val("1");
     $('#menge').select();
   }
@@ -124,6 +169,83 @@ function usevariant(id)
     variationId = id;
     menge();
 }
+
+function einbuchen()
+{
+  $('#load').show();
+  var locationean = $('.locationEan').val();
+  var x = locationean.split("L");
+  var error = 0;
+  if (typeof x[1] != 'undefined')
+  {
+    var xx = x[1].split("S");
+  }
+  else {
+    var x = locationean.split("l");
+    if (typeof x[1] != 'undefined')
+    {
+      var xx = x[1].split("s");
+    }
+    else {
+      error = 1;
+    }
+  }
+
+  if(error === 0)
+  {
+    var lager = xx[0];
+    //TODO die Lager checkboxen beachten
+    var location = xx[1];
+    console.log(lager);
+    console.log(location);
+    var qty = $('#menge').val();
+    var date = new Date();
+    date = date.toW3CString();
+    console.log(date);
+    //date = "2017-05-15T03:30:29-04:00";
+    var url = "/rest/stockmanagement/warehouses/"+
+            lager+"/stock/bookIncomingItems";
+
+    console.log(url);
+    $.ajax({
+          type: "PUT",
+          url: url,
+          headers: {
+      			"Authorization": "Bearer "+localStorage.getItem("accessToken")
+      		},
+          data: { incomingItems: [
+            {
+                variationId: variationId,
+                currency: "EUR",
+                reasonId: 101,
+                quantity: qty,
+                storageLocationId: location,
+                deliveredAt: date
+            }
+          ]},
+          success: function(data)
+          {
+            $('#load').hide();
+            $('#output').html("<div class='green'><p>Artikel wurde erfolgreich eingebucht</p></div>");
+            $('.locationEan').prop("disabled", true);
+            $('.locationEan').val("");
+            $('.findArticle').removeAttr("disabled");
+            $('.findArticle').select();
+          },
+          error: function(data)
+          {
+            $('#load').hide();
+            console.log(data);
+            alert("EIN UNBEKANNTER FEHLER IST AUFGETRETEN");
+          }
+      });
+  }
+  else {
+    $('#output').html("<div class='find-false'><p>Bitte überprüfen Sie Ihre eingabe!</p></div>");
+    $('#load').hide();
+    $('.locationEan').select();
+  }
+}
 $(document).ready(function(){
   checkaccess();
 
@@ -149,7 +271,15 @@ $(document).ready(function(){
       login();
     }
   });
+  $('#menge').bind("keydown", function(e)
+  {
+    if(e.keyCode === 13)
+    {
+      $('.locationEan').focus();
+    }
+  });
   $('.back').click( function(){
+    $(this).prop("disabled", true);
     $('#output').html("");
     $('.locationEan').val("");
     $('.findArticle').val("");
@@ -158,4 +288,11 @@ $(document).ready(function(){
     $('.findArticle').focus();
   });
 
+  $('.locationEan').bind("keydown", function(e)
+  {
+    if(e.keyCode === 13)
+    {
+      einbuchen();
+    }
+  });
 });
