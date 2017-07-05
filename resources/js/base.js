e936ee98a0fd1cf8241c770bc143aaa33a5a695d
domee1337 <a2@fs24.local>
2017-07-05 09:32:31 +0200

    sx

--- resources/js/base.js
+++ resources/js/base.js
@@ -298,30 +298,6 @@ $(document).ready(function(){
       login();
     }
   });
-  $('#menge').bind("keydown", function(e)
-  {
-    if(e.keyCode === 13)
-    {
-      $('.locationEan').focus();
-    }
-  });
-  $('.back').click( function(){
-    $(this).prop("disabled", true);
-    $('#output').html("");
-    $('.locationEan').val("");
-    $('.findArticle').val("");
-    $('.locationEan').prop("disabled", true);
-    $('.findArticle').removeAttr("disabled");
-    $('.findArticle').focus();
-  });
-
-  $('.locationEan').bind("keydown", function(e)
-  {
-    if(e.keyCode === 13)
-    {
-      einbuchen();
-    }
-  });
 
   $(document).ajaxStart( function(){
     $('#load').fadeIn(100);
--- resources/js/start.js
+++ resources/js/start.js
@@ -7,4 +7,29 @@ $(document).ready( function(){
             var result = findVariant($(this).val());
           }
   });
+  $('#menge').bind("keydown", function(e)
+  {
+    if(e.keyCode === 13)
+    {
+      $('.locationEan').focus();
+    }
+  });
+  $('.back').click( function(){
+    $(this).prop("disabled", true);
+    $('#output').html("");
+    $('.locationEan').val("");
+    $('.findArticle').val("");
+    $('.locationEan').prop("disabled", true);
+    $('.findArticle').removeAttr("disabled");
+    $('.findArticle').focus();
+  });
+
+  $('.locationEan').bind("keydown", function(e)
+  {
+    if(e.keyCode === 13)
+    {
+      einbuchen();
+    }
+  });
+
 });
